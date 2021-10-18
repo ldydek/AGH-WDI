eb442d04b4f1fc7de8c4f44aa8a2c492e1bc1598
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-18 23:02:21 +0200

    Add files via upload

---
+++ WDI/Set 1/ex.3.py
@@ -0,0 +1,20 @@
+# 3. Write a program that checks if contiguous subsequence summing up to certain number exists in a Fibonacci
+# sequence. Idea is to iterate from the beginning and when temporary sum exceeds our number, we start subtracting
+# elements of the Fibonacci sequence from the beginning (because subsequence has to be contiguous).
+def ex3(k):
+    suma = 0
+    a, b, p, q = 0, 1, 0, 1
+    while a <= k:
+        if suma == k:
+            return True
+        suma += a
+        a, b = b, a + b
+        if suma > k:
+            while suma > k:
+                suma -= p
+                p, q = q, p + q
+    return False
+
+
+print(ex3(3))
+
