aaf1b2c9257d6059f3d0ce1b6b8913a4ac78be50
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-19 00:12:14 +0200

    Update ex.6.py

--- WDI/Set 1/ex.6.py
+++ WDI/Set 1/ex.6.py
@@ -1,10 +1,10 @@
 # 6. Write program solving equation x^x = 2020 by bisection method.
-# At first we initialize variables for 0 and for 5, because 5^5 = 3125 and is bigger than 2020. Later if
-# ((0+5)/2)**((0+5)/2) is bigger than 2020 or not we change variable "a" or "b" and repeat the same until we achieve
-# given accuracy or find suitable number.
+# At first we initialize variables for 4 and for 5, because 4^4 = 256 is smaller and 5^5 = 3125 is bigger than 2020.
+# Later if ((0+5)/2)**((0+5)/2) is bigger than 2020 or not we change variable "a" or "b" and repeat the same until we
+# achieve given accuracy or find a solution.
 
 def ex6():
-    a, b, accuracy = 0, 5, 0.0001
+    a, b, accuracy = 4, 5, 0.0001
     while b-a > accuracy:
         x = (a+b)/2
         if x**x == 2020:
