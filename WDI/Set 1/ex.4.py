9be76aad526995e8b26226507c2ded61b342f947
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-18 23:37:20 +0200

    Add files via upload

---
+++ WDI/Set 1/ex.4.py
@@ -0,0 +1,14 @@
+# 4. Write program which computes integer part of a square root of a given number, using following:
+# 1^2 + 3^2 + 5^2 + ... = n^2.
+# Solution: when our temporary sum will exceed given number, solution is quantity of while loop performances.
+
+def ex4(n):
+    ctr, suma, x = -1, 0, 1
+    while suma <= n:
+        suma += x
+        x += 2
+        ctr += 1
+    return ctr
+
+
+print(ex4(345))
