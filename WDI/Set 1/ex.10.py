68697f613b5d3db5302b2857d2570355a98d1b87
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-19 20:19:29 +0200

    Add files via upload

---
+++ WDI/Set 1/ex.10.py
@@ -0,0 +1,22 @@
+# 10. Print all perfect numbers smaller than million.
+
+
+def is_perfect(k):
+    suma, i = 0, 2
+    while i**2 < k:
+        if k % i == 0:
+            suma += i + k//i
+        i += 1
+    suma += 1
+    if suma == k:
+        return True
+    return False
+
+
+def ex10():
+    for x in range(2, 10**5):
+        if is_perfect(x):
+            print(x, end=" ")
+
+
+ex10()
