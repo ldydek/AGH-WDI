2a48a00e6a1e454569de2d727f6e69e49196fb68
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-24 13:25:31 +0200

    Update ex.5.py

--- WDI/Set 1/ex.5.py
+++ WDI/Set 1/ex.5.py
@@ -1,4 +1,7 @@
 # 5. Write program computing square root of a given number using Newton's formula.
+# Solution: We can think about it in this way: at first we have a rectangle 1xn, where "n" is
+# our given number. Our goal is to change length of rectangle sides to obtain a square, but
+# its surface has to be the same all the time.
 
 
 def ex5(n):
