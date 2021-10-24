1d12681f479b6a5ea0a27358b4eba2f3ae2bafcf
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-24 13:37:40 +0200

    Add files via upload

---
+++ WDI/Set 1/ex.18.py
@@ -0,0 +1,13 @@
+# 18. Modify program from exercise 5. Now it has to compute cube root.
+# Solution: Same idea but this time not for a rectangle but for cuboid.
+
+def ex18(n):
+    a, b, c, e = 1, 1, n, 10**(-10)
+    while abs(c-a) > e:
+        a = (a+c)/2
+        b = (b+c)/2
+        c = n/(a*b)
+    return c
+
+
+print(ex18(125))
