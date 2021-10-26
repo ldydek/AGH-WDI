4f77d8c0df5e63d15ef1439c956f45e0f60e4193
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-26 12:06:44 +0200

    Add files via upload

---
+++ WDI/Set 2/ex.3.py
@@ -0,0 +1,40 @@
+# 3. Write program that informs us whether entered number is a palindrome and later whether it is a palindrome in
+# binary number system.
+
+
+def is_palindrome(n, k):
+    number = 0
+    for x in range(k):
+        k -= 1
+        number += ((n // (10**x)) % 10) * (10**k)
+    return number == n
+
+
+def binary(n):
+    number, ctr = 0, 0
+    while n > 0:
+        number *= 10
+        number += n % 2
+        n //= 2
+        ctr += 1
+    return number, ctr
+# Note that this function starts showing proper digits from last nonzero element, but for determining if binary
+# representation is a palindrome it's sufficient knowing additionally number of digits in binary system.
+
+
+def length(n):
+    ctr = 0
+    while n > 0:
+        n //= 10
+        ctr += 1
+    return ctr
+
+
+def ex3(n):
+    k = length(n)
+    print(is_palindrome(n, k))
+    a, b = binary(n)
+    print(is_palindrome(a, b))
+
+
+ex3(121)
