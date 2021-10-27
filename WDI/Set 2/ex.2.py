c00e85ff4866b766a388dc595183d959a61b483e
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-28 01:11:08 +0200

    Update ex.2.py

--- WDI/Set 2/ex.2.py
+++ WDI/Set 2/ex.2.py
@@ -2,14 +2,12 @@
 # "n" digits after a point.
 
 def ex2(a, b, n):
-    ctr = 0
     print(a//b, end=".")
-    while ctr < n:
+    while n > 0:
         a = (a % b)*10
         print(a//b, end="")
-        ctr += 1
+        n -= 1
 
 
 ex2(12, 7, 20)
-
 
