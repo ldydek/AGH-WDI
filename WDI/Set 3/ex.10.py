0012a26c2d7ac79c446c2aaeeac481c21a43c1fc
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-29 19:36:37 +0200

    Add files via upload

---
+++ WDI/Set 3/ex.10.py
@@ -0,0 +1,17 @@
+# 10. Write program which determines the length of the longest contiguous arithmetic sequence.
+
+def ex10(arr):
+    n, solution, ctr = len(arr), 0, 2
+    for x in range(1, n-1):
+        k = arr[x]-arr[x-1]
+        if arr[x+1] - arr[x] == k:
+            ctr += 1
+        else:
+            solution = max(solution, ctr)
+            ctr = 2
+    solution = max(solution, ctr)
+    return solution
+
+
+arr = [2, 5, 3, 6, 9, 12]
+print(ex10(arr))
