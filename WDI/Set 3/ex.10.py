bc8133d3181d50ceec8af2e2e91d7f7f6fe80221
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-29 19:51:27 +0200

    Update ex.10.py

--- WDI/Set 3/ex.10.py
+++ WDI/Set 3/ex.10.py
@@ -1,7 +1,9 @@
 # 10. Write program which determines the length of the longest contiguous arithmetic subsequence in a given array.
 
 def ex10(arr):
-    n, solution, ctr = len(arr), 0, 2
+    n, solution, ctr = len(arr), 2, 2
+    if n == 1:
+        return 1
     for x in range(1, n-1):
         k = arr[x]-arr[x-1]
         if arr[x+1] - arr[x] == k:
