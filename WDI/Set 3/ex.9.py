b611740554c9a5d9e69641377ccc4ea9b755af9d
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-29 15:30:37 +0200

    Add files via upload

---
+++ WDI/Set 3/ex.9.py
@@ -0,0 +1,15 @@
+# 9. Write program which determines the length of the longest contiguous increasing subsequence.
+
+def ex9(arr):
+    n, solution, ctr = len(arr), 0, 1
+    for x in range(n-1):
+        if arr[x] < arr[x+1]:
+            ctr += 1
+            solution = max(solution, ctr)
+        else:
+            ctr = 1
+    return solution
+
+
+arr = [1, 1, 2, 3, 2, 1, 10]
+print(ex9(arr))
