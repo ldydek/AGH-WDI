4e8ee5181f93c1c236af1acd96311f37293f63d5
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-03 14:12:56 +0100

    Update ex.1.py

--- WDI/Set 3/ex.1.py
+++ WDI/Set 3/ex.1.py
@@ -1,5 +1,5 @@
 # 1. Write program changing given number to any number system from 2 to 16.
-# "n" - given number, "a" - number system
+# "n" - given number, "a" - base of number system
 
 def ex1(n, a):
     arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
@@ -11,3 +11,5 @@ def ex1(n, a):
 
 
 print(ex1(1000, 16))
+# Alternatively I could allocate an array starting from "A" not from "0" and use
+# conversion to string.
