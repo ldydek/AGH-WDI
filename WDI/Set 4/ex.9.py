a607b3b9d8b9d5066d861ece71e3018ffcb3f0d3
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-03 09:49:07 +0100

    Add files via upload

---
+++ WDI/Set 4/ex.9.py
@@ -0,0 +1,19 @@
+# 9. We have a given 2d array with natural numbers. Write program checking whether we can find in it a square with odd
+# number of elements bigger than 1 for which product of four corner elements is equal to "k".
+
+
+def ex9(arr, k):
+    n = len(arr)
+    for x in range(n):
+        for y in range(n):
+            ctr = 2
+            while x + ctr < n and y + ctr < n:
+                p = arr[x][y]*arr[x+ctr][y]*arr[x][y+ctr]*arr[x+ctr][y+ctr]
+                if p == k:
+                    return x+ctr//2, y+ctr//2
+                ctr += 2
+    return False
+
+
+arr = [[5, 1, 10, 3, 1], [1, 7, 1, 7, 8], [3, 2, 1, 9, 8], [4, 6, 4, 10, 7], [5, 7, 3, 1, 5]]
+print(ex9(arr, 150))
