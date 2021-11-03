d6e168aca5b428ce9bc3789aca8bdc713e1cf73a
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-03 14:26:40 +0100

    Update ex.2.py

--- WDI/Set 3/ex.2.py
+++ WDI/Set 3/ex.2.py
@@ -1,5 +1,8 @@
 # 2. Write program checking whether given numbers are built with the same digits e.g. 123 and 312. I suppose here that
 # 776661123 is built with digits: 7,6,1,2,3 and the same goes with 31276 (so repeats are not important).
+# Solution: In the array we maintain information which digit we met so far. Later during researching second number
+# we change these numbers from 1 to 2 not to 0, because later if we meet the same digit condition in line 13 could
+# be satisfied. This trick can help us solving the problem with only one auxiliary array.
 
 def ex2(a, b):
     arr = [0]*10
