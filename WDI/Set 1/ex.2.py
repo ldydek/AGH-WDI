12e224e4a52d81911989c0705f57e50b32efe750
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-04 16:17:34 +0100

    Update ex.2.py

--- WDI/Set 1/ex.2.py
+++ WDI/Set 1/ex.2.py
@@ -1,5 +1,10 @@
 # 2. Find beginning elements instead of 1, 1 of minimum sum that analogous sequence to Fibonacci sequence will contain
 # an element equal to this year number.
+# Solution: I check every pair of elements from 1 to floor(a/2) without it and for that pair I start creating sequence
+# similar to Fibonacci sequence. "a" - year, "b" - our sum that we are looking for, "c", "d", "xd", "ty" - beginning
+# values of considered pair of numbers. One pair is moving to check if 2020 year is in this sequence. If so, we then
+# use "c" and "b" to check if c+b is smaller than our temporary sum. If so I assign these starting numbers to my
+# solution ("an", "bn") and modify temporary sum.
 
 def ex2():
     a, b = 2020, 2020
