2ea17363920e255c972842e7e9c1ff8f2d43ee60
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-04 16:53:27 +0100

    Update ex.9.py

--- WDI/Set 1/ex.9.py
+++ WDI/Set 1/ex.9.py
@@ -1,4 +1,6 @@
 # 9. Write program printing all given number divisors.
+# Solution: We iterate in range from 1 to floor of square root plus one and when we meet a number divisible by "n",
+# when "n" is our given number we can print this divisor, let's say "x", and n/x at once.
 
 
 def ex9(n):
