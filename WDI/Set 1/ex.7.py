62c5c121ecd76fd12d33cbeb51efef09486c1cc9
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-04 16:47:35 +0100

    Update ex.7.py

--- WDI/Set 1/ex.7.py
+++ WDI/Set 1/ex.7.py
@@ -1,6 +1,6 @@
 # 7. Write program that checks if given number is a product of two neighbouring elements in a Fibonacci sequence.
 # Solution: We iterate through this sequence and when product of two contiguous elements exceeds given number it means
-# that we can't reach this number, but when it's equal it means this number is available.
+# that we can't reach this number, but when it's equal it means that this number is available.
 
 
 def ex7(n):
