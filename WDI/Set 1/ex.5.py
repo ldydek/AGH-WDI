ca42016db6af7a219cdca2e57f33736b3dc21f0b
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-04 16:44:41 +0100

    Update ex.5.py

--- WDI/Set 1/ex.5.py
+++ WDI/Set 1/ex.5.py
@@ -2,6 +2,8 @@
 # Solution: We can think about it in this way: at first we have a rectangle 1xn, where "n" is
 # our given number. Our goal is to change length of rectangle sides to obtain a square, but
 # its surface has to be the same all the time.
+# "a", "b" - dimensions and "e" - as square root can be irrational we let an acceptable difference between "a" and "b"
+# when we can break the loop.
 
 
 def ex5(n):
