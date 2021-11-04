9b03d1ee71486d676cb10a7ba0774e9672b337be
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-04 16:06:55 +0100

    Update ex.1.py

--- WDI/Set 1/ex.1.py
+++ WDI/Set 1/ex.1.py
@@ -1,4 +1,6 @@
 # 1. Print all Fibonacci sequence elements smaller than million.
+# Solution: I use here convenient change of variable values that Python gives us. If variable "a" value will exceed
+# 10**6 while loop will be broken.
 
 def zad1():
     a, b = 1, 1
