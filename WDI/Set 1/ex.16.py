bd633b5a6c13741ef54c9b85e5408946816e1237
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-06 12:29:54 +0100

    Update ex.16.py

--- WDI/Set 1/ex.16.py
+++ WDI/Set 1/ex.16.py
@@ -1,6 +1,9 @@
 # 16. There is a sequence of a given formula: An+1 = (An % 2) * (3 * An + 1) + (1 - An % 2) * An/2. Beginning from
 # any natural number n > 1 one of the sequence elements will reach value 1 anyway. Write program finding starting
 # element from range (2-10000) for which value 1 is accessible after greatest number of steps.
+# Solution: I explicitly consider every number in range from 2 to 10000 and check for each one after how many steps
+# value 1 will be accessible. Variables "solution" and "xd" keep information about best element and biggest number
+# of steps found so far during for loop action.
 
 
 def ex16():
