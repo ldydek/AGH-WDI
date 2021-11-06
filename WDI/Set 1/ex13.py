5065331c7565bd7ed78e7c7f640205e081a95c1e
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-06 11:51:27 +0100

    Update ex13.py

--- WDI/Set 1/ex13.py
+++ WDI/Set 1/ex13.py
@@ -1,16 +1,18 @@
 # 13. Write program printing the least common multiple (LCM) of three given numbers.
-# Solution: LCM(x, y) = x*y / GCD(x, y) (greatest common divisor)
+# Solution: LCM(x, y) = x*y / GCD(x, y) (greatest common divisor). Note that computing LCM of three numbers as max
+# of LCM(x, y), LCM(x, z) and LCM(y, z) is incorrect, because e.g. for 101, 201 and 4 max(LCM(101, 201), LCM(201, 4),
+# LCM(101, 4)) is 20301. We have to do it in this way: LCM(x, y, z) = LCM(LCM(x, y), z), so in this way it's 81204.
 
 
-def product_by_gcd(a, b):
+def lcm(a, b):
     x, y = a, b
-    while a:
+    while a != 0:
         a, b = b % a, a
     return (x*y)//b
 
 
 def ex13(a, b, c):
-    return product_by_gcd(product_by_gcd(a, b), c)
+    return lcm(lcm(a, b), c)
 
 
 print(ex13(101, 201, 4))
