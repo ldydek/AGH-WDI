f7326b6c7723624e3fac464cc13b4cc1aa80bfdf
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-06 12:40:20 +0100

    Update ex.19.py

--- WDI/Set 1/ex.19.py
+++ WDI/Set 1/ex.19.py
@@ -1,4 +1,6 @@
 # 19. Write program computing value of "e" number using: e = 1/0! + 1/1! + 1/2! + 1/3! + ... .
+# Solution: I sum first 100 elements of this sequence to compute "e" value. Note that here simultaneously with adding
+# new elements to the sum I can compute x! value, where "x" is a loop counter.
 
 def ex19():
     a, factorial = 1, 1
