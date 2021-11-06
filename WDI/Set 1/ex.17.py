51f847e2e7e023bb32edfa2585950cab217aed30
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-06 12:34:24 +0100

    Update ex.17.py

--- WDI/Set 1/ex.17.py
+++ WDI/Set 1/ex.17.py
@@ -1,5 +1,7 @@
 # 17. Write program computing the value which quotient of two Fibonacci sequence neighbouring elements approaches.
 # Determine this quotient for different beginning elements of this sequence.
+# Solution: During for loop running program prints quotient of two neighbouring elements. We can note that this
+# quotient approaches to a certain number which is called golden number.
 
 
 def ex17():
