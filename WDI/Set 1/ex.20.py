de9d15879996a6efe419d3245ac819786de7490d
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-06 12:43:25 +0100

    Update ex.20.py

--- WDI/Set 1/ex.20.py
+++ WDI/Set 1/ex.20.py
@@ -1,5 +1,7 @@
 # 20. Given two sequences: An+1 = sqrt(An * Bn) and Bn+1 = (An + Bn)/2. They are convergent to the common limit called
 # arithmetic-geometric mean. Write program computing this mean.
+# Solution: During while loop action I assign constantly new values to the variables and when given accuracy is gained
+# I can just return either "an" or "bn", because these values are almost the same.
 
 def ex20():
     an, bn, e = 24, 6, 10**(-10)
