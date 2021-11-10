516315c81c851ccc382b50328a10a5709ad832d7
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-10 11:36:52 +0100

    Update ex.6.py

--- WDI/Set 4/ex.6.py
+++ WDI/Set 4/ex.6.py
@@ -3,19 +3,23 @@
 # increasingly.
 
 
-def ex6(arr):
-    n = len(arr)
-    arr1 = [0]*(n**2)
+def ex6(T1):
+    n = len(T1)
+    T2 = [0]*(n**2)
     for x in range(n):
         for y in range(n):
-            arr1[x*n+y] = arr[x][y]
-    arr1.sort()
+            T2[x*n+y] = T1[x][y]
+    T2.sort()
 
     for x in range(n**2-1):
-        if arr1[x] == arr1[x+1]:
-            arr1[x] = 0
-    arr1.sort()
-    return arr1
+        if T2[x] == T2[x+1]:
+            T2[x] = 0
+    T2.sort()
+    return T2
+
+
+T1 = [[1, 5, 8, 9], [1, 4, 7, 8], [3, 4, 5, 10], [3, 7, 11, 13]]
+print(ex6(T1))
 
 
 arr = [[1, 5, 8, 9], [1, 4, 7, 8], [3, 4, 5, 10], [3, 7, 11, 13]]
