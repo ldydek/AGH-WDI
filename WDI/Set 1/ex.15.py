9e4b8e932434edb9fe60661302e69b54460ae183
ldydek <91955435+ldydek@users.noreply.github.com>
2021-11-11 00:59:47 +0100

    Update ex.15.py

--- WDI/Set 1/ex.15.py
+++ WDI/Set 1/ex.15.py
@@ -7,8 +7,8 @@ from math import pi
 
 
 def ex15():
-    product, e, eps = 1, (1/2)**(1/2), 10**(-10)
-    while abs(2/pi - product) > eps:
+    product, e = 1, (1/2)**(1/2)
+    while abs(2/pi - product) > 10**(-10):
         product *= e
         e = (1/2 + e/2)**(1/2)
     return 2/product
